5a9c72f8f9b5d8128da50a671bf2beabcae8a8e5
Llistoman <josepll.plamarcell@gmail.com>
2016-03-13 20:20:57 +0100

    Representacion del estado, es muy basica, faltaran cosas supongo. He hecho mi version y la de Felix.

---
+++ src/representacion/State_1.java
@@ -0,0 +1,42 @@
+package representacion;
+
+import java.util.ArrayList;
+
+public class State_1 {
+    private int ns;
+    private ArrayList<ArrayList<Integer>> servers;
+
+    public State_1(int s) {
+        ns = s;
+        servers = new ArrayList<>(s);
+    }
+
+    //Operators
+
+    public void addRequest(int server_id, int req) {
+        servers.get(server_id).add(req);
+    }
+
+    public void moveRequest(int server1, int server2, int req) {
+        int n = servers.get(server1).indexOf(req);
+        servers.get(server1).remove(n);
+        servers.get(server2).add(req);
+    }
+
+    public void swapRequest(int server1, int r1, int server2, int r2) {
+        int i = servers.get(server1).indexOf(r1);
+        int j = servers.get(server2).indexOf(r2);
+        servers.get(server1).remove(i);
+        servers.get(server2).remove(j);
+        servers.get(server1).add(i,r2);
+        servers.get(server2).add(j,r1);
+    }
+
+    //Getters
+
+    public int getNs() { return ns; }
+
+    public ArrayList<Integer> getAllRequests(int server_id) {
+        return servers.get(server_id);
+    }
+}
---
+++ src/representacion/State_2.java
@@ -0,0 +1,35 @@
+package representacion;
+
+import java.util.ArrayList;
+
+public class State_2 {
+    private int nr;
+    private ArrayList<Integer> requests;
+
+    public State_2(int n) {
+        nr = n;
+        requests = new ArrayList<>(n);
+    }
+
+    //Operators
+
+    public void setServer(int request_id, int server_id) {
+        requests.set(request_id,server_id);
+    }
+
+    public void swapServers(int r1, int r2) {
+        int s1 = requests.get(r1);
+        int s2 = requests.get(r2);
+        requests.set(r1,s2);
+        requests.set(r2,s1);
+    }
+
+    //Getters
+
+    public int getNr() { return nr; }
+
+    public int serverOf(int request_id) {
+        return requests.get(request_id);
+    }
+
+}
