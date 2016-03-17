00543000095bce6047426eeeaa497334ddcea83b
vylion <volfaria@gmail.com>
2016-03-17 12:37:16 +0100

    La lista de sucesores agregaba el estado sin variar, en vez del nuevo estado. Fixed. El error de recorrer solo un nodo sigue en pie.

--- src/representacion/ServerSuccessorFunction.java
+++ src/representacion/ServerSuccessorFunction.java
@@ -25,7 +25,7 @@ public class ServerSuccessorFunction implements SuccessorFunction {
                         if (!data.isPossibleMove(i, j, k)) continue;
                         ServerData dataNew = new ServerData(data);
                         dataNew.moveRequest(i, j, k);
-                        list.add(new Successor(String.format("move i %d j %d k %d",i,j,k), data));
+                        list.add(new Successor(String.format("move i %d j %d k %d",i,j,k), dataNew));
                     }
                 }
             }    
