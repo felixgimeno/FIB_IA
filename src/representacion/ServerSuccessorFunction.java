132a10eeb71cea5455de10677ba99bba3281236a
Llistoman <josepll.plamarcell@gmail.com>
2016-03-29 17:54:45 +0200

    lo dejo solo con moves que funciona muy bien

--- src/representacion/ServerSuccessorFunction.java
+++ src/representacion/ServerSuccessorFunction.java
@@ -21,24 +21,24 @@ public class ServerSuccessorFunction implements SuccessorFunction {
             for (Integer j = 0; j < data.getNservers(); j +=1 ){
                 if (!Objects.equals(j, i)){
                     for (Integer k : (ArrayList<Integer>)data.getRequests(i)){
-                        /*if (!data.isPossibleMove(i, j, k)) continue;
+                        if (!data.isPossibleMove(i, j, k)) continue;
                         ServerData dataNew = new ServerData(data);
                         dataNew.moveRequest(i, j, k);
-                        list.add(new Successor(String.format("move s1 %d s2 %d req %d",i,j,k), dataNew));*/
+                        list.add(new Successor(String.format("move s1 %d s2 %d req %d",i,j,k), dataNew));
 
                         //añadir intercambios
-                        for (Integer l : (ArrayList<Integer>)data.getRequests(j)) {
+                        /*for (Integer l : (ArrayList<Integer>)data.getRequests(j)) {
                             if (data.isPossibleSwap(i, k, j, l)) {
                                 ServerData dataNew2 = new ServerData(data);
                                 dataNew2.swapRequest(i, k, j, l);
                                 list.add(new Successor(String.format("swap s1 %d req1 %d s2 %d req2 %d", i, k, j, l), dataNew2));
                             }
-                            else if (!data.isPossibleMove(i, j, k)) {
+                            else if (data.isPossibleMove(i, j, k)) {
                                 ServerData dataNew = new ServerData(data);
                                 dataNew.moveRequest(i, j, k);
                                 list.add(new Successor(String.format("move s1 %d s2 %d req %d",i,j,k), dataNew));
                             }
-                        }
+                        }*/
                     }
                 }
             }    
