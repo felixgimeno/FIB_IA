21692883666094a07a6a252a57bb3afe60b586aa
felix.axel.gimeno <felix.axel.gimeno@10.10.53.78>
2016-04-07 15:47:25 +0200

    random seed

--- src/representacion/ServerSuccessorFunctionR.java
+++ src/representacion/ServerSuccessorFunctionR.java
@@ -13,22 +13,33 @@ import java.util.Random;
  */
 public class ServerSuccessorFunctionR implements SuccessorFunction {
 
+    private int saSeed;
+    public ServerSuccessorFunctionR(int saSeed){
+        this.saSeed=saSeed;
+    }
     @Override
     public List getSuccessors(Object state) {
         final ServerData data = (ServerData) state;
         List<Successor> list = new ArrayList<> ();
         for (int count = 0; count < 100; count += 1){
-            int i = new Random().nextInt(data.getNservers());
+            final int i = new Random(this.saSeed).nextInt(data.getNservers());
             if (data.getRequests(i).isEmpty()) {
                 continue;
             }
-            int j = new Random().nextInt(data.getNservers());
-            int k = new Random().nextInt(data.getRequests(i).size());
+            final int j = new Random(this.saSeed).nextInt(data.getNservers());
+            final int k = new Random(this.saSeed).nextInt(data.getRequests(i).size());
+            
+            final boolean moveSelected = 1 == new Random(this.saSeed).nextInt(1+data.getNreq());
+            
             if (!data.isPossibleMove(i, j, k)) continue;
             final ServerData dataNew = new ServerData(data);
             dataNew.moveRequest(i, j, k);
             list.add(new Successor(String.format("move i %d j %d k %d",i,j,k), dataNew));
             return list;
+            //move far es numberServers^2 * numberRequests
+            //swap far es numberServers^2 * numberRequests^2
+            // swap far / move far = numberRequests
+
         }
         assert(!list.isEmpty());
         //System.out.println(list);
